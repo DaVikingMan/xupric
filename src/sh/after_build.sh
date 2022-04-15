814eade8e15065a16e4d871980e41c69aeb9e5c7
DaVikingMan <shinshiro1@tutanota.com>
2022-04-15 12:30:21 +0700

    Fix script

--- src/sh/after_build.sh
+++ src/sh/after_build.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
+# Note : These files are only to be ran inside the current folder, if it's ran outside it, it wouldn't work
 
 mkdir -p ~/.config/xupric/styles/
 
 cd ../
-cp -n xupric.conf ~/.config/xupric/
-cp -n src/css/main.css ~/.config/xupric/styles/
-cp -n src/css/dark_mode.css ~/.config/xupric/styles/
-cp -n src/css/scrollbar.css ~/.config/xupric/styles/
+cp -n ../xupric.conf ~/.config/xupric/
+cp -n css/main.css ~/.config/xupric/styles/
+cp -n css/dark_mode.css ~/.config/xupric/styles/
+cp -n css/scrollbar.css ~/.config/xupric/styles/
